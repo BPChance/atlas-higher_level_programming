da484794eb5b88fb365a94a26294337eb0b9b459
root <bradenpchance6@gmail.com>
2024-05-07 01:20:41 +0000

    now it gotta s

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,12 @@ if __name__ == "__main__":
     num_arguments = len(argv) - 1
 
     if num_arguments == 0:
-        print(".", end='\n\n')
+        print("0 arguments.", end='\n')
+    elif num_arguments > 1:
+        print("{} arguments:".format(num_arguments), end='\n')
+
+        for i, arg in enumerate(argv[1:], start=1):
+            print("{}: {}".format(i, arg))
     else:
         print("{} argument:".format(num_arguments), end='\n')
 
