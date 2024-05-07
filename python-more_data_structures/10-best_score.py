a3444d9475fc0e350f61f4c686586805533b026e
root <bradenpchance6@gmail.com>
2024-05-07 22:09:55 +0000

    more data structures

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    best_key = None
+    best_score = float('-inf')
+
+    for key, value in a_dictionary.items():
+        if value > best_score:
+            best_score = value
+            best_key
+    return best_key
