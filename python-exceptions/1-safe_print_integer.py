8dbc1c1a62a4a960d3e149ded2d3f6d9f55c19f2
root <bradenpchance6@gmail.com>
2024-05-08 03:16:38 +0000

    fix?

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,6 @@ def safe_print_integer(value):
     try:
         if isinstance(value, int):
             print("{:d}".format(value))
-            print()
             return True
     except ValueError:
         return False
