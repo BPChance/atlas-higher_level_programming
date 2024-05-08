7e2516b89bd60f70499013d0330ab97635765f77
root <bradenpchance6@gmail.com>
2024-05-08 03:30:20 +0000

    formatted wrong

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end='')
-                count += 1
-            except (ValueError, TypeError):
-                pass
-    except IndexError:
-        pass
-    finally:
-        print()
-        if x > len(my_list):
-            raise IndexError("Index out of range")
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            count += 1
+        except (ValueError, TypeError, IndexError):
+            pass
+    print()
+    if x > len(my_list):
+        raise IndexError("Index out of range")
     return count
