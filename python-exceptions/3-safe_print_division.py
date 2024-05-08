8dcd1661211e2db34e804158267cef13843c8387
root <bradenpchance6@gmail.com>
2024-05-08 03:44:28 +0000

    result?

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,6 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Cannot divide by zero")
         return None
     except Exception as e:
         print("An error occured:", e)
