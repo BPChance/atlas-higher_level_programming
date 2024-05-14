8ecd6e27f147bbb806b483ba0fb56b8c8cad04ab
BPChance <bradenpchance6@gmail.com>
2024-05-13 20:20:43 -0500

    added delete to Rectangle

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -52,7 +52,7 @@ class Rectangle:
 
     def __repr__(self):
         return f"Rectangle({self.width}, {self.height})"
-    
+
     def __del__(self):
         """get rid of rectangle"""
         print("Bye rectangle...")
