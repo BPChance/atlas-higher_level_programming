74daba17dda082993a5b7669ffd97cafa86c9484
BPChance <bradenpchance6@gmail.com>
2024-05-13 19:24:40 -0500

    fixed spaces

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -35,9 +35,8 @@ class Rectangle:
     def area(self):
         """calculates and returns the area"""
         return self.width * self.height
-    
+
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
-    
