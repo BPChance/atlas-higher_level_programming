b0316666d24143408b59d7fca64f40ccadb65b21
BPChance <bradenpchance6@gmail.com>
2024-05-13 19:51:37 -0500

    rectangle that prints #

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -47,6 +47,6 @@ class Rectangle:
         rectangle = ""
         for i in range(self.height):
             rectangle += "#" * self.width
-            if i < self.height - 1
+            if i < self.height - 1:
             rectangle += "\n"
         return rectangle
