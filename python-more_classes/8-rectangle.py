9a02ecb634f52b3d0145c1d1de92671649c99918
BPChance <bradenpchance6@gmail.com>
2024-05-13 21:22:15 -0500

    added 8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -2,14 +2,12 @@
 """defines a class"""
 
 
-
 class Rectangle:
     """defines a rectangle"""
 
 
     number_of_instances = 0
     print_symbol = "#"
-
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
