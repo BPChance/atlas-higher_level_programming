2a1db699a3311f9a7ee86f746f87f0c5ce285dff
BPChance <bradenpchance6@gmail.com>
2024-05-20 16:29:12 -0500

    class that inherits from 7

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3
+"""module containing rectangle class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """class representing Rectangle"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
