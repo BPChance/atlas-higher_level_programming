a32a1f3d1960f040dbb5bb3980157b83d5c84cb8
BPChance <bradenpchance6@gmail.com>
2024-05-21 20:26:50 -0500

    changed f to file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """define a function"""
-    with open(filename, "w", encoding="utf-8") as f:
-        json.dump(my_obj, f)
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(my_obj, file)
