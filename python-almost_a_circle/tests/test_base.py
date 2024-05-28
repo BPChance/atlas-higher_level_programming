2fd3c99a0e47e69089e89f486f76dcdfc5169129
BPChance <bradenpchance6@gmail.com>
2024-05-28 15:04:59 -0500

    unittest for base class

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -8,78 +8,74 @@ class TestBase(unittest.TestCase):
         """Reset the __nb_objects counter before each test"""
         Base._Base__nb_objects = 0
 
-    def test_base_id_none(self):
-        """Test creating a Base instance with id=None"""
+    def test_base_auto_id(self):
+        """Test for automatically assigned IDs"""
         b1 = Base()
         b2 = Base()
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-
-    def test_base_id_provided(self):
-        """Test creating a Base instance with a specific id"""
-        b1 = Base(10)
-        b2 = Base(20)
-        self.assertEqual(b1.id, 10)
-        self.assertEqual(b2.id, 20)
-
-    def test_base_id_mixed(self):
-        """Test creating Base instances with and without id"""
-        b1 = Base()
-        b2 = Base(15)
         b3 = Base()
         self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 15)
-        self.assertEqual(b3.id, 2)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
 
-    def test_to_json_string(self):
-        """Test converting a list of dictionaries to a JSON string"""
-        list_dicts = [{'id': 1}, {'id': 2}]
-        json_str = Base.to_json_string(list_dicts)
-        self.assertIsInstance(json_str, str)
-        self.assertEqual(json_str, '[{"id": 1}, {"id": 2}]')
+    def test_base_custom_id(self):
+        """Test for custom ID assignment"""
+        b1 = Base(89)
+        self.assertEqual(b1.id, 89)
 
     def test_to_json_string_none(self):
-        """Test converting None to a JSON string"""
-        json_str = Base.to_json_string(None)
-        self.assertIsInstance(json_str, str)
-        self.assertEqual(json_str, '[]')
+        """Test JSON string conversion for None"""
+        self.assertEqual(Base.to_json_string(None), "[]")
 
-    def test_to_json_string_empty(self):
-        """Test converting an empty list to a JSON string"""
-        json_str = Base.to_json_string([])
-        self.assertIsInstance(json_str, str)
-        self.assertEqual(json_str, '[]')
+    def test_to_json_string_empty_list(self):
+        """Test JSON string conversion for empty list"""
+        self.assertEqual(Base.to_json_string([]), "[]")
 
-    def test_from_json_string(self):
-        """Test converting a JSON string to a list of dictionaries"""
-        json_str = '[{"id": 1}, {"id": 2}]'
-        list_dicts = Base.from_json_string(json_str)
-        self.assertIsInstance(list_dicts, list)
-        self.assertEqual(list_dicts, [{'id': 1}, {'id': 2}])
+    def test_to_json_string(self):
+        """Test JSON string conversion for a valid list of dictionaries"""
+        dict_list = [{'id': 12}, {'id': 34}]
+        json_str = Base.to_json_string(dict_list)
+        self.assertEqual(json_str, '[{"id": 12}, {"id": 34}]')
+        self.assertIsInstance(json_str, str)
 
     def test_from_json_string_none(self):
-        """Test converting None to an empty list"""
-        list_dicts = Base.from_json_string(None)
-        self.assertIsInstance(list_dicts, list)
-        self.assertEqual(list_dicts, [])
+        """Test conversion from JSON string when None is provided"""
+        self.assertEqual(Base.from_json_string(None), [])
 
-    def test_from_json_string_empty(self):
-        """Test converting an empty string to an empty list"""
-        list_dicts = Base.from_json_string("")
-        self.assertIsInstance(list_dicts, list)
-        self.assertEqual(list_dicts, [])
+    def test_from_json_string_empty_list(self):
+        """Test conversion from JSON string for empty list"""
+        self.assertEqual(Base.from_json_string("[]"), [])
 
-    def test_create(self):
-        """Test creating an instance from a dictionary"""
-        dictionary = {'id': 1}
-        base_instance = Base.create(**dictionary)
-        self.assertEqual(base_instance.id, 1)
+    def test_from_json_string(self):
+        """Test conversion from JSON string for a valid JSON string"""
+        json_str = '[{"id": 89}, {"id": 90}]'
+        dict_list = Base.from_json_string(json_str)
+        self.assertEqual(dict_list, [{"id": 89}, {"id": 90}])
+        self.assertIsInstance(dict_list, list)
+
+    def test_save_to_file(self):
+        """Test saving list of Base objects to a file"""
+        b1 = Base(1)
+        b2 = Base(2)
+        list_objs = [b1, b2]
+        Base.save_to_file(list_objs)
+        with open("Base.json", "r") as file:
+            content = file.read()
+            self.assertEqual(Base.from_json_string(content), [{'id': 1}, {'id': 2}])
 
     def test_load_from_file_no_file(self):
-        """Test loading instances from a non-existent file"""
-        instances = Base.load_from_file()
-        self.assertIsInstance(instances, list)
-        self.assertEqual(instances, [])
+        """Test loading from a file that does not exist"""
+        self.assertEqual(Base.load_from_file(), [])
+
+    def test_load_from_file(self):
+        """Test loading list of Base objects from a file"""
+        b1 = Base(1)
+        b2 = Base(2)
+        list_objs = [b1, b2]
+        Base.save_to_file(list_objs)
+        loaded_objs = Base.load_from_file()
+        self.assertEqual(len(loaded_objs), 2)
+        self.assertEqual(loaded_objs[0].id, 1)
+        self.assertEqual(loaded_objs[1].id, 2)
 
 if __name__ == '__main__':
     unittest.main()
