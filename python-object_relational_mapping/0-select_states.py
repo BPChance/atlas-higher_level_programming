a1be4d7b82387f30333b7ee44f6f5ab8895a4425
BPChance <bradenpchance6@gmail.com>
2024-06-18 12:59:55 -0500

    script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
 def list_all_states(username, password, database):
+    """connects to database and lists all states from the database hbtn_0e_0_us"""
     conn = MySQLdb.connect(
         host='localhost',
         port=3306,
