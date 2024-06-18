90eb1b2e634e40329f3319a97b3e2eaf450355d0
BPChance <bradenpchance6@gmail.com>
2024-06-18 09:35:57 -0500

    sql script

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- prints description of a table from a database
-SELECT COLUMN_NAME, COLUMN_TYPE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
