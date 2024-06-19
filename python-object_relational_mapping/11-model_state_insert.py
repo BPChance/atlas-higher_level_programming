432cb8cf5ce8d5610307fe161dafc11cfe3cf5a6
BPChance <bradenpchance6@gmail.com>
2024-06-19 16:59:34 -0500

    adding 11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -6,18 +6,20 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
+
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        username, password, database = sys.argv[1:4]
+    username, password, database = sys.argv[1:4]
 
-        engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
 
-        new_state = State(name="Louisiana")
-        session.add(new_state)
-        session.commit()
-        
-        print(new_state.id)
 
-        session.close()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
+
+    session.close()
