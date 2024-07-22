88935f2bd606a36cf6fda917ecd67897be6a73b4
BPChance <bradenpchance6@gmail.com>
2024-07-22 18:39:29 -0500

    script that prints My number: <first argument converted in integer>

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const lines = [
+    'C is fun',
+    'Python is cool',
+    'JavaScript is amazing'
+];
+
+let result = '';
+
+for (const line of lines) {
+    result += line + '\n';
+}
+
+console.log(result.trim());
