6d202648d09358d5ed6753429344f12bd6260486
BPChance <bradenpchance6@gmail.com>
2024-07-22 19:41:14 -0500

    semistandard

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,9 +4,9 @@ const arg = process.argv[2];
 const times = parseInt(arg, 10);
 
 if (isNaN(times)) {
-    console.log('Missing number of occurences');
+  console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < times; i++) {
-        console.log('C is fun');
-    }
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
+  }
 }
