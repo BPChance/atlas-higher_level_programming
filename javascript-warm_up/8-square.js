42b2c8342a240978c6cb20dc0af5ae308c36ec7f
BPChance <bradenpchance6@gmail.com>
2024-07-22 19:42:32 -0500

    semistandard

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,9 +4,9 @@ const arg = process.argv[2];
 const size = parseInt(arg, 10);
 
 if (isNaN(size) || size <= 0) {
-    console.log('Missing size')
+  console.log('Missing size');
 } else {
-    for (let i = 0; i < size; i++) {
-        console.log('X'.repeat(size));
-    }
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 }
