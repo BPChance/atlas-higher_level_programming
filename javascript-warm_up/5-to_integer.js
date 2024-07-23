23f1028aaa52c16a18ca9b2529122c2a1abbfa7b
BPChance <bradenpchance6@gmail.com>
2024-07-22 19:25:45 -0500

    fixed else output

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const arg = process.argv[2];
 const number = parseInt(arg, 10);
 
 if (isNaN(number)) {
-    console.log(`Not a number`);
+    console.log('Not a number');
 } else {
     console.log(`My number: ${number}`);
 }
