765cdca120d156fdc1e08eef731d7fd4992da348
BPChance <bradenpchance6@gmail.com>
2024-07-22 19:43:46 -0500

    fixed error output

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -9,7 +9,7 @@ const num1 = parseInt(arg1, 10);
 const num2 = parseInt(arg2, 10);
 
 if (isNaN(num1) || isNaN(num2)) {
-    console.log('Invalid input');
+    console.log('NaN');
 } else {
     console.log(add(num1, num2));
 }
