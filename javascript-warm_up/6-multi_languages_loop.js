2c5710d6b950db2dd3e55d582f7641c4fb16fc97
BPChance <bradenpchance6@gmail.com>
2024-07-22 19:35:10 -0500

    semistandard

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 const lines = [
-    `C is fun`,
-    `Python is cool`,
-    `JavaScript is amazing`
-];
-let result = ``;
-for (const line of lines) {
+    'C is fun',
+    'Python is cool',
+    'JavaScript is amazing'
+  ];
+  let result = '';
+  for (const line of lines) {
     result += line + '\n';
-}
-
-console.log(result.trim());
+  }
+  
+  console.log(result.trim());
+  
