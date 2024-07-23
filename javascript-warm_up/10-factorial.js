bed660fc26306a5526f1728a319c9c7349a0c70a
BPChance <bradenpchance6@gmail.com>
2024-07-22 20:00:16 -0500

    10 semistandard

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-function factorial(n) {
+function factorial (n) {
     if (n <= 1) return 1;
     return n * factorial(n - 1);
-}
+  }
 
-const arg = process.argv[2];
-const num = parseInt(arg, 10);
+  const arg = process.argv[2];
+  const num = parseInt(arg, 10);
 
-if (isNaN(num)) {
+  if (isNaN(num)) {
     console.log(1);
-} else {
+  } else {
     console.log(factorial(num));
-}
+  }
