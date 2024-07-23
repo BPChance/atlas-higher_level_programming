e11bf840756a23a7863d17aac3dda5696e74d8c0
BPChance <bradenpchance6@gmail.com>
2024-07-22 19:11:31 -0500

    fix print

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-const arg1 = process.argv[0];
-const arg2 = process.argv[1];
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 
 console.log(`${arg1} is ${arg2}`);
