d308f66258d281036abdf1e4be9df739ba8aa0c3
root <bradenpchance6@gmail.com>
2024-07-24 02:31:27 +0000

    using height instead of size

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -4,14 +4,14 @@ const Square = require('./5-square');
 
 class SquareExtend extends Square {
   constructor (size) {
-    super(size);
+    super(size, size);
   }
 
   charPrint (c) {
     const char = c || 'X';
-    if (this.size) {
-      for (let i = 0; i < this.size; i++) {
-        console.log(char.repeat(this.size));
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(char.repeat(this.width));
       }
     }
   }
