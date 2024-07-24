3ae8909b155428bc1a13bc00ef6cd46cb31b5289
root <bradenpchance6@gmail.com>
2024-07-24 02:17:32 +0000

    parsing error and useless container

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,18 +3,18 @@
 const Square = require('./5-square');
 
 class SquareExtend extends Square {
-    constructor(size) {
-        super(size);
-    }
+  constructor (size) {
+    super(size);
+  }
 
-    charPrint(c) {
-        const char = c || 'X';
-        if (this.size) {
-            for (let i = 0; i < this.size; i++) {
-                console.log(char.repeat(this.size));
-            }
-        }
+  charPrint (c) {
+    const char = c || 'X';
+    if (this.size) {
+      for (let i = 0; i < this.size; i++) {
+        console.log(char.repeat(this.size));
+      }
     }
+  }
 }
 
 module.exports = SquareExtend;
