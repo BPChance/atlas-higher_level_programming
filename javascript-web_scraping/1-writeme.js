cd895aeff94fd0e8fd72f9a532502483346ea48b
BPChance <bradenpchance6@gmail.com>
2024-07-28 13:12:51 -0500

    fixed permissions?

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
+fs.writeFile(filePath, content, 'utf8', (err) => {
   if(err) {
     console.error('Error writing to file', 'utf-8', err);
   } else {
