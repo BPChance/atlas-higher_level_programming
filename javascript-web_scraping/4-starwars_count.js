fad6ab8769e180774883e4ae3ccab08551c807a6
BPChance <bradenpchance6@gmail.com>
2024-07-29 19:24:50 -0500

    prints the number of movies where the character “Wedge Antilles” is present

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+const request = require('request');
+const baseUrl = 'https://swapi-api.hbtn.io/api/films/';
+const characterID = 18;
+
+function countWedgeAppearances(url) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    const nextUrl = data.next;
+
+    let count = 0;
+    for (const film of data.results) {
+      for (const characterUrl of film.characters) {
+        if (characterUrl.endWith(`/${characterID}`)) {
+          count++;
+          break;
+        }
+      }
+    }
+
+    console.log(count);
+
+    if (nextUrl) {
+      countWedgeAppearances(nextUrl);
+    }
+  });
+}
+
+countWedgeAppearances(baseUrl);
